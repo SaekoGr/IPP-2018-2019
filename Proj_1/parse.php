274c1a67529787c32a7ca0219b12670d60eafd57
Sabína Gregušová <sabina.gregusova@gmail.com>
2019-02-16 23:36:18 +0100

    I hope it's done

--- Proj_1/parse.php
+++ Proj_1/parse.php
@@ -37,16 +37,34 @@ function write_instruction_header($opcode){
     xmlwriter_text($GLOBALS['xml'], strtoupper($opcode));
 }
 
-# instruction has no arguments
-function zero_args($opcode, $line){
-    if("$opcode\n" == $line or "$opcode" == $line){
-        write_instruction_header($opcode);
-        xmlwriter_end_element($GLOBALS['xml']);
-        return 0;
+# 
+function has_comment($line){
+    if(strpos($line, '#') !== false){
+        return true;
     }
     else{
+        return false;
+    }
+}
+
+function remove_comment($first_part, $line, $remove_from){
+    $comment_regex = "/.*(?=#.*)/";
+    preg_match($comment_regex, $remove_from, $match);
+    $is_ok = false;
+    if(!$match){
         return 23;
     }
+    $before_comment = $match[0];
+
+    while(!$is_ok){
+        if($before_comment[strlen($before_comment) - 1] == " " or $before_comment[strlen($before_comment) - 1] == "\t"){
+            $before_comment = substr($before_comment, 0, -1);
+        }
+        else{
+            $is_ok = true;
+        }
+    }
+    return $before_comment;
 }
 
 # writes argument
@@ -59,10 +77,13 @@ function write_arg($type, $arg_num, $arg){
     xmlwriter_end_element($GLOBALS['xml']);
 }
 
-# 
+# it gets the type in front of the '@' 
 function get_type($arg){
     $type_regex = "/.*(?=@)/";
     preg_match($type_regex, $arg, $match);
+    if(!$match){
+        return "error";
+    }
     $type = $match[0];
 
     if($type == "GF" or $type == "LF" or $type == "TF"){
@@ -88,7 +109,7 @@ function has_at($arg){
     }
 }
 
-#
+# gets everything after '@'
 function after_at($arg){
     if(has_at($arg)){
         $after_regex = "/(?<=@).*/";
@@ -100,9 +121,8 @@ function after_at($arg){
     }
 }
 
-#
-function evaluate_arg($opcode, $arg, $arg_type, $arg_num){
-    write_instruction_header($opcode);
+# evaluates arguments, opcodes, sorts it into the xml format
+function evaluate_arg($arg, $arg_type, $arg_num){
     if($arg_type == "symb"){
         $arg_type = get_type($arg);
         if($arg_type == "error"){
@@ -117,7 +137,6 @@ function evaluate_arg($opcode, $arg, $arg_type, $arg_num){
             }
             else{
                 write_arg($arg_type, $arg_num, $current_string);
-                xmlwriter_end_element($GLOBALS['xml']);
                 return 0;
             }
         case "label":   # no
@@ -127,7 +146,6 @@ function evaluate_arg($opcode, $arg, $arg_type, $arg_num){
             }
             else{
                 write_arg($arg_type, $arg_num, $arg);
-                xmlwriter_end_element($GLOBALS['xml']);
             }
             break;
         case "var":
@@ -135,7 +153,6 @@ function evaluate_arg($opcode, $arg, $arg_type, $arg_num){
         case "int":
         case "nil":
             write_arg($arg_type, $arg_num, $arg);
-            xmlwriter_end_element($GLOBALS['xml']);
             return 0;
             break;
         default:
@@ -143,22 +160,133 @@ function evaluate_arg($opcode, $arg, $arg_type, $arg_num){
     }
 }
 
+# instruction has no arguments
+function zero_args($opcode, $line){
+    if(has_comment($line)){
+        $last_part = remove_comment($opcode, $line, $line);
+        write_instruction_header($last_part);
+        xmlwriter_end_element($GLOBALS['xml']);
+    }
+    elseif("$opcode\n" == $line or "$opcode" == $line){
+        write_instruction_header($opcode);
+        xmlwriter_end_element($GLOBALS['xml']);
+        return 0;
+    }
+    else{
+        return 23;
+    }
+}
+
 #
 function one_arg($opcode, $line, $arg1_type){
+    # find first argument
     $arg1_regex = "/(?<=$opcode ).*/";
     preg_match($arg1_regex, $line, $match);
+
+    # look whether we even found it, we have to have 1 argument
     if(!$match){
         return 23;
     }
     $arg1 = $match[0];
-    $is_ok = False;
 
-    if("$opcode $arg1\n" != $line and "$opcode $arg1" == $line){
+    # check whether it matches
+    if(has_comment($line)){
+        $last_part = remove_comment($arg1, $line, $arg1);
+        write_instruction_header($opcode);
+        evaluate_arg($last_part, $arg1_type, "arg1");
+        xmlwriter_end_element($GLOBALS['xml']);
+    }
+    elseif("$opcode $arg1\n" == $line or "$opcode $arg1" == $line){
+        write_instruction_header($opcode);
+        evaluate_arg($arg1, $arg1_type, "arg1");
+        xmlwriter_end_element($GLOBALS['xml']);
+    }
+    else{
         return 23;
     }
+}
 
-    evaluate_arg($opcode, $arg1, $arg1_type, "arg1");
+#
+function two_args($opcode, $line, $arg1_type, $arg2_type){
+    # first argument
+    $arg1_regex = "/(?<=$opcode ).*?(?= )/";
+    preg_match($arg1_regex, $line, $match_a);
+    if(!$match_a){
+        return 23;
+    }
+    $arg1 = $match_a[0];
 
+    # second argument
+    $arg2_regex = "/(?<=$opcode $arg1 ).*/";
+    preg_match($arg2_regex, $line, $match_b);
+    if(!$match_b){
+        return 23;
+    }
+    $arg2 = $match_b[0];
+
+    if(has_comment($line)){
+        $last_part = remove_comment($arg2, $line, $arg2);
+        write_instruction_header($opcode);
+        evaluate_arg($arg1, $arg1_type, "arg1");
+        evaluate_arg($last_part, $arg2_type, "arg2");
+        xmlwriter_end_element($GLOBALS['xml']);
+    }
+    elseif("$opcode $arg1 $arg2\n" == $line or "$opcode $arg1 $arg2" == $line){
+        write_instruction_header($opcode);
+        evaluate_arg($arg1, $arg1_type, "arg1");
+        evaluate_arg($arg2, $arg2_type, "arg2");
+        xmlwriter_end_element($GLOBALS['xml']);
+    }
+    else{
+        return 23;
+    }
+}
+
+# 
+function three_args($opcode, $line, $arg1_type, $arg2_type, $arg3_type){
+    # first argument
+    $arg1_regex = "/(?<=$opcode ).*?(?= )/";
+    preg_match($arg1_regex, $line, $match_a);
+
+    if(!$match_a){
+        return 23;
+    }
+    $arg1 = $match_a[0];
+
+    # second argument
+    $arg2_regex = "/(?<=$opcode $arg1 ).*?(?= )/";
+    preg_match($arg2_regex, $line, $match_b);
+    if(!$match_b){
+        return 23;
+    }
+    $arg2 = $match_b[0];
+
+    # third argument
+    $arg3_regex = "/(?<=$opcode $arg1 $arg2 ).*/";
+    preg_match($arg3_regex, $line, $match_c);
+    if(!$match_c){
+        return 23;
+    }
+    $arg3 = $match_c[0];
+
+    if(has_comment($line)){
+        $last_part = remove_comment($arg3, $line, $arg3);
+        write_instruction_header($opcode);
+        evaluate_arg($arg1, $arg1_type, "arg1");
+        evaluate_arg($arg2, $arg2_type, "arg2");
+        evaluate_arg($last_part, $arg3_type, "arg3");
+        xmlwriter_end_element($GLOBALS['xml']);
+    }
+    elseif("$opcode $arg1 $arg2 $arg3\n" == $line or "$opcode $arg1 $arg2 $arg3" == $line){
+        write_instruction_header($opcode);
+        evaluate_arg($arg1, $arg1_type, "arg1");
+        evaluate_arg($arg2, $arg2_type, "arg2");
+        evaluate_arg($arg3, $arg3_type, "arg3");
+        xmlwriter_end_element($GLOBALS['xml']);
+    }
+    else{
+        return 23;
+    }
 }
 
 #
@@ -207,80 +335,102 @@ function compare_opcode($opcode, $line){
         return one_arg($opcode, $line, "symb");
     }
     elseif(!strcasecmp($opcode, "MOVE")){           # 2
-        
+        return two_args($opcode, $line, "var", "symb");
     }
     elseif(!strcasecmp($opcode, "INT2CHAR")){       # 2
-        
+        return two_args($opcode, $line, "var", "symb");
     }
     elseif(!strcasecmp($opcode, "READ")){           # 2
-        
+        return two_args($opcode, $line, "var", "type");
     }
     elseif(!strcasecmp($opcode, "STRLEN")){         # 2
-        
+        return two_args($opcode, $line, "var", "symb");
     }
     elseif(!strcasecmp($opcode, "TYPE")){           # 2
-        
+        return two_args($opcode, $line, "var", "symb");
     }
     elseif(!strcasecmp($opcode, "ADD")){            # 3
-        
+        return three_args($opcode, $line, "var", "symb", "symb");
     }
     elseif(!strcasecmp($opcode, "SUB")){            # 3
-        
+        return three_args($opcode, $line, "var", "symb", "symb");
     }
     elseif(!strcasecmp($opcode, "MUL")){            # 3
-        
+        return three_args($opcode, $line, "var", "symb", "symb");
     }
     elseif(!strcasecmp($opcode, "IDIV")){           # 3
-        
+        return three_args($opcode, $line, "var", "symb", "symb");
     }
     elseif(!strcasecmp($opcode, "LT")){             # 3
-        
+        return three_args($opcode, $line, "var", "symb", "symb");
     }
     elseif(!strcasecmp($opcode, "GT")){             # 3
-        
+        return three_args($opcode, $line, "var", "symb", "symb");
     }
     elseif(!strcasecmp($opcode, "EQ")){             # 3
-        
+        return three_args($opcode, $line, "var", "symb", "symb");
     }
     elseif(!strcasecmp($opcode, "AND")){            # 3
-        
+        return three_args($opcode, $line, "var", "symb", "symb");
     }
     elseif(!strcasecmp($opcode, "OR")){             # 3
-        
+        return three_args($opcode, $line, "var", "symb", "symb");
     }
     elseif(!strcasecmp($opcode, "NOT")){            # 3
-        
+        return three_args($opcode, $line, "var", "symb", "symb");
     }
     elseif(!strcasecmp($opcode, "STRI2INT")){       # 3
-        
+        return three_args($opcode, $line, "var", "symb", "symb");
     }
     elseif(!strcasecmp($opcode, "CONCAT")){         # 3
-        
+        return three_args($opcode, $line, "var", "symb", "symb");
     }
     elseif(!strcasecmp($opcode, "GETCHAR")){        # 3
-        
+        return three_args($opcode, $line, "var", "symb", "symb");
     }
     elseif(!strcasecmp($opcode, "SETCHAR")){        # 3
-        
+        return three_args($opcode, $line, "var", "symb", "symb");
     }
     elseif(!strcasecmp($opcode, "JUMPIFEQ")){       # 3
-        
+        return three_args($opcode, $line, "label", "symb", "symb");
     }
     elseif(!strcasecmp($opcode, "JUMPIFNEQ")){      # 3
-        
+        return three_args($opcode, $line, "label", "symb", "symb");
     }
     else{
+        
         return 22;
     }
     return 0;
 }
 
+# look whether the line is comment
+function is_one_line_comment($line){
+    $comment_regex = "/[ \t]*#.*/";
+    preg_match($comment_regex, $line, $match);
+    if(!$match){
+        return false;
+    }
+    elseif("$match[0]" == "$line" or "$match[0]\n" == "$line"){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
 # processes each line
 function process_line($line){
+    # check for one line comment
+    if(is_one_line_comment($line)){
+        return 0;
+    }
+
     $opcode_regex = "/^[a-zA-Z]*/";
     preg_match($opcode_regex, $line, $match);
     $opcode = $match[0];
     return compare_opcode($opcode, $line);
+
 }
 
 # functions for help
